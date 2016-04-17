24ddd852f63384f831053e300284fe5d2257895a
贾永恒 <jia_yh@outlook.com>
2016-04-17 22:40:42 +0800

    添加项目

---
+++ src/main/java/cn/edu/tju/scs/spm/my_app_simple/App.java
@@ -0,0 +1,17 @@
+package cn.edu.tju.scs.spm.my_app_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(new App().welcome("World"));
+    }
+    
+    public String welcome(String name) {
+		return "Hello " + name + "!";
+	}
+}
---
+++ src/test/java/cn/edu/tju/scs/spm/my_app_simple/AppTest.java
@@ -0,0 +1,19 @@
+package cn.edu.tju.scs.spm.my_app_simple;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+
+
+	@Test
+	public void testWelcome() {
+		App app = new App();
+		String result = app.welcome("a");
+		assertEquals(result, "Hello a!");
+	}
+}
