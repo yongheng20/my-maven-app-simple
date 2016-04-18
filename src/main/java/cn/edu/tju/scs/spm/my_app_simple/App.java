f0296e5c22cf98d3fcac8198234652f988433eca
cindy <zjtstudyt@126.com>
2016-04-18 15:08:22 +0800

    ZJT

--- src/main/java/cn/edu/tju/scs/spm/my_app_simple/App.java
+++ src/main/java/cn/edu/tju/scs/spm/my_app_simple/App.java
@@ -12,6 +12,7 @@ public class App
         System.out.println(new App().welcome("Li Yuze"));
         System.out.println(new App().welcome("Zheng Yifeng"));
         System.out.println(new App().welcome("Wang Jingwen"));        
+        System.out.println(new App().welcome("Zhao Junting"));
     }
     
     public String welcome(String name) {
